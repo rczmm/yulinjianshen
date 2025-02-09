78f6ff184b5a20df3a1abaa22bab4738ed2c7d60
rczmm <3179623450@qq.com>
2025-02-09 16:42:40 +0800

    feat(webapp): 创建健身小程序项目
    - 初始化项目配置和目录结构
    - 添加首页组件和样式- 集成 NutUI组件库
    - 配置 ESLint 和 TypeScript
    - 添加 Git忽略文件和编辑器配置

---
+++ src/components/RCard/RCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <view class="card-container" :style="{ backgroundImage: `url(${backgroundImage})` }">
+    <view class="card-content">
+      <view class="title-description">
+        <text class="title">{{ title }}</text>
+        <text class="description">{{ description }}</text>
+      </view>
+
+      <view class="list-container">
+        <view class="list-item" v-for="(item, index) in listData" :key="index">
+          <image class="item-image" :src="item.image" mode="aspectFill" />
+          <view class="item-info">
+            <text class="item-title">{{ item.title }}</text>
+            <view class="item-details">
+              <text class="detail-text">{{ item.time }}</text>
+              <text class="detail-text">{{ item.energy }}</text>
+              <text class="detail-text">{{ item.level }}</text>
+            </view>
+          </view>
+        </view>
+        <view class="view-more-button" v-if="listData.length > 0">
+          <button class="button-text">查看更多</button>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import './RCard.scss';
+
+
+const props = defineProps({
+  backgroundImage: String,
+  title: String,
+  description: String,
+  listData: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <view class="index">
+    <nut-searchbar></nut-searchbar>
+
+    <swiper>
+      <swiper-item>1</swiper-item>
+      <swiper-item>2</swiper-item>
+      <swiper-item>3</swiper-item>
+    </swiper>
+
+    <nut-grid :column-num="5">
+
+      <nut-grid-item v-for="(item, index) in 10" :key="index" :text="item">
+        <text>1</text>
+      </nut-grid-item>
+
+
+    </nut-grid>
+
+    <nut-divider></nut-divider>
+
+    <RCard
+      backgroundImage="https://i1.hdslb.com/bfs/archive/cb398a597840785835f2eb608389f0c5146bb864.jpg"
+      title="卡片标题"
+      description="卡片描述"
+      :listData="cardListData"
+    ></RCard><RCard
+      backgroundImage="https://i1.hdslb.com/bfs/archive/cb398a597840785835f2eb608389f0c5146bb864.jpg"
+      title="卡片标题"
+      description="卡片描述"
+      :listData="cardListData"
+    ></RCard><RCard
+      backgroundImage="https://i1.hdslb.com/bfs/archive/cb398a597840785835f2eb608389f0c5146bb864.jpg"
+      title="卡片标题"
+      description="卡片描述"
+      :listData="cardListData"
+    ></RCard>
+    <RCard
+      backgroundImage="https://i1.hdslb.com/bfs/archive/cb398a597840785835f2eb608389f0c5146bb864.jpg"
+      title="卡片标题"
+      description="卡片描述"
+      :listData="cardListData"
+    ></RCard>
+
+  </view>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+import RCard from "../../components/RCard/RCard.vue";
+
+const show = ref(false)
+const onClick = () => {
+  show.value = true
+}
+
+const cardListData = ref([
+  {
+    image: 'https://i1.hdslb.com/bfs/archive/cb398a597840785835f2eb608389f0c5146bb864.jpg',
+    title: '列表项标题1',
+    time: '时间1',
+    energy: '能量1',
+    level: '等级1',
+  },
+  {
+    image: 'https://i1.hdslb.com/bfs/archive/cb398a597840785835f2eb608389f0c5146bb864.jpg',
+    title: '列表项标题2',
+    time: '时间2',
+    energy: '能量2',
+    level: '等级2',
+  }
+]);
+
+</script>
