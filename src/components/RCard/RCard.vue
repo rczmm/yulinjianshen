52193b0f7e4a7430562f62938bf17f056fed4488
rcz <3179623450@qq.com>
2025-03-15 12:40:13 +0800

    feat(course): 丰富课程页面内容和功能
    - 添加课程详情、评论和动作列表
    - 实现课程数据异步加载
    -优化页面样式和布局
    - 新增导航到专题页面的功能
    - 移除未使用的 NutIcon组件引用

--- src/components/RCard/RCard.vue
+++ src/components/RCard/RCard.vue
@@ -31,7 +31,13 @@
 <script setup>
 import { defineProps } from 'vue';
 import './RCard.scss';
+import Taro from '@tarojs/taro';
 
+const navToTopic = (title) => {
+  Taro.navigateTo({
+    url: `/pages/topic/index?title=${title}`,
+  });
+};
 
 const props = defineProps({
   backgroundImage: String,
